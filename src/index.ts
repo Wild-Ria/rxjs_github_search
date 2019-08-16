2bb1d151275fd5b7d6534393d92f52975eb5695d
Marianna <marianna@loopme.com>
2019-08-16 18:36:30 +0300

    refactoring

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,29 @@
-import { fromEvent } from 'rxjs';
-import { switchMap, debounceTime } from 'rxjs/operators';
+import { fromEvent, from, Observable } from 'rxjs';
+import { switchMap, debounceTime, distinctUntilChanged, map, filter, pluck } from 'rxjs/operators';
 
 
 const inputElement: HTMLInputElement = document.querySelector('input') as HTMLInputElement;
-const resultElement: HTMLDivElement = document.querySelector('#result') as HTMLDivElement;
+const resultElement: HTMLDivElement = document.querySelector('.result') as HTMLDivElement;
 
 const input$ = fromEvent(inputElement, 'input');
 
-// @ts-ignore
-input$.pipe(debounceTime(600), switchMap((event: KeyboardEvent) => {
-    return fetch(`https://api.github.com/search/repositories?q=${(event.target as HTMLInputElement).value}`)
-        .then(response => response.json());
-})).subscribe((response: any) => response.items.map((item: any) => {
-    resultElement.insertAdjacentHTML("beforeend", `<li><a href="${item.html_url}">${item.full_name}</a></li>`)
-}));
+function request(param: string): Observable<any> {
+    return from(fetch(`https://api.github.com/search/repositories?q=${param}`)
+        .then((res: Response) => res.json()));
+}
+
+const inputS$ = input$.pipe(
+    debounceTime(500),
+    map(event => event.target.value),
+    filter(event => event),
+    distinctUntilChanged(),
+    switchMap(request),
+    pluck('items'),
+);
+
+inputS$.subscribe(res => {
+    resultElement.innerHTML = '';
+    res.map(item => {
+        resultElement.insertAdjacentHTML('beforeend', `<li>${item.name} <a href="${item.url}">${item.url}</a></li>`);
+    })
+});
